7fde81acd08ba341128e149bd8575fdf85893040
doill <rndemr@gmail.com>
2022-02-11 11:16:17 +0900

    ddddd

--- src/main/java/com/sesoc/team2/controller/BookController.java
+++ src/main/java/com/sesoc/team2/controller/BookController.java
@@ -18,39 +18,7 @@ import com.sesoc.team2.dao.BookDBMapper;
 public class BookController {
 	private static final Logger logger = LoggerFactory.getLogger(BookController.class);
 	
-	//책 상세정보
-	@RequestMapping (value="bookinfo", method=RequestMethod.GET)
-	public String bookinfoForm() {
-		return "bookinfoForm";
-	}
 	
-	@Autowired
-	private BookDBMapper bookDBMapper;
-	
-	
-	public static int page_size = 100;
-	public static String url = "https://openapi.naver.com/v1/search/book.xml";
-	public static String service_key = "fL7oGeaMDyKTcSQ8K9E7";
-	
-	public void update() throws Exception {
-		int result = 0;
-		List<Map<String, String>> list = new ArrayList<>();
-		
-//		try {
-//		//책 목록 읽어오기
-//		createDocument(list);
-//		
-//			for (Map<String, String> bookInfo : list) {
-//			createDetailInfo(bookInfo);
-//			
-//			result += 
-//			}
-//		}
-	}
-	
-	private void parseDetailXml(Element root, Map<String, String> map) {
-		
-	}
 	
 	
 }
