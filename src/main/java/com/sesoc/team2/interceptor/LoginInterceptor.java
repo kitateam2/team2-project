e4d13ac433ede1656db6637bd231df17888dd3bc
hyunjung_jay <jaykim9237@gmail.com>
2022-02-16 17:07:02 +0900

    인터셉터

--- src/main/java/com/sesoc/team2/interceptor/LoginInterceptor.java
+++ src/main/java/com/sesoc/team2/interceptor/LoginInterceptor.java
@@ -1,17 +1,23 @@
 package com.sesoc.team2.interceptor;
 
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 public class LoginInterceptor extends HandlerInterceptorAdapter {
+	private static final Logger logger = LoggerFactory.getLogger(LoginInterceptor.class);
 
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
 		
+		logger.info("LoginInterceptor 실행");
+	
 		HttpSession session = request.getSession();
 		String loginId = (String)session.getAttribute("loginId");
 		
