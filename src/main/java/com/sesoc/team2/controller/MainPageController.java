e7ff7bef61a6412d99fd1056e3413a8fe08c5ae0
hyunjung_jay <jaykim9237@gmail.com>
2022-02-21 11:30:02 +0900

    홈 컨트롤러 변경 // 페이지 넘기는 것

--- src/main/java/com/sesoc/team2/controller/MainPageController.java
+++ src/main/java/com/sesoc/team2/controller/MainPageController.java
@@ -1,6 +1,7 @@
 package com.sesoc.team2.controller;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,8 +11,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.sesoc.team2.dao.BlogPostDAO;
+import com.sesoc.team2.util.PageNavigator;
 import com.sesoc.team2.vo.BlogPost;
 
 
@@ -26,13 +29,38 @@ public class MainPageController {
 	@Autowired
 	BlogPostDAO dao;
 	
-	@RequestMapping(value = "myblog/{user_id}", method = RequestMethod.GET)
-	public String myblog(@PathVariable String user_id, Model model) {
-		ArrayList<BlogPost> postlist =dao.postlist(user_id);
-		logger.info("DB에서 가져온 목록{}", postlist);
-		model.addAttribute("postlist", postlist);
-		model.addAttribute("user_id", user_id);
-		return "myblog/myblogMain";
-	}
+	//게시판 관련 상수값들
+		final int countPerPage = 10;			//페이지당 글 수
+		final int pagePerGroup = 5;				//페이지 이동 링크를 표시할 페이지 수
+	
+		@RequestMapping(value = "myblog/{user_id}", method = RequestMethod.GET)
+		public String myblog(@PathVariable String user_id,
+							@RequestParam(value="page", defaultValue="1") int page
+							, @RequestParam(value="searchText", defaultValue="") String searchText
+							,Model model) {
+			
+			logger.debug("page: {}, searchText: {}", page, searchText);
+			int total = dao.get_total(searchText);//전체 글 개수
+			
+			//페이지 계산을 위한 객체 생성
+			//                                     여기 10개        여기 5개      1개   100개 
+			PageNavigator navi = new PageNavigator(countPerPage, pagePerGroup, page, total); 
+			
+			//해시맵
+			HashMap<String, String> map = new HashMap<String, String>();
+			
+			map.put("user_id", user_id);
+			map.put("searchText", searchText);
+					
+			logger.info("컨트롤러 확인 map{}", map);
+			ArrayList<BlogPost> postlist =dao.postlist(map, navi.getStartRecord(), navi.getCountPerPage());
+			logger.info("DB에서 가져온 목록{}", postlist);
+			
+			model.addAttribute("postlist", postlist);
+			model.addAttribute("user_id", user_id);
+			model.addAttribute("navi", navi);
+			model.addAttribute("searchText", searchText);
+			return "myblog/myblogMain";
+		}
 	
 }
